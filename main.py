2e148daa5e4b3a9aeeaf932cbfa0fee4ad8ad8d6
Jainil Patel <jainil.patel.btech2021@sitpune.edu.in>
2023-04-28 10:37:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ import streamlit as st
 
 
 import subprocess
-subprocess.run(['curl --output model_1.h5 "https://drive.google.com/uc?export=download&id=1TLvqnFG-jsh-fKECpXei8snjKhcskoLk"'], shell=True)
+subprocess.run(['curl --output model_1.h5 "https://github.com/AMITESH30/WEB_deploy/blob/main/final_model.h5?raw=true"'], shell=True)
 
 # Define the CNN model
 model = tf.keras.models.load_model("model_1.h5" ,compile=False)
